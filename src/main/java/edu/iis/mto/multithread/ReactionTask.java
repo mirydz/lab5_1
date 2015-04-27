da37edb3b04825d3fcf7b21781c923dcc63c9cf5
Michal Rydz <mirydz@gmail.com>
2015-04-27 10:50:53 +0200

    remove unused field

--- src/main/java/edu/iis/mto/multithread/ReactionTask.java
+++ src/main/java/edu/iis/mto/multithread/ReactionTask.java
@@ -3,11 +3,9 @@ package edu.iis.mto.multithread;
 
 public class ReactionTask implements Runnable {
 
-    private Runnable task;
     private PatriotBattery battery;
 
-    public ReactionTask(Runnable task, PatriotBattery battery) {
-        this.task = task;
+    public ReactionTask(PatriotBattery battery) {
         this.battery = battery;
     }
 
